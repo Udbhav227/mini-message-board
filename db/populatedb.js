d9d16e656758ce6f99eac8ca46fe4899de9d3a76
Udbhav <udbhav227@gmail.com>
2026-03-22 07:15:29 +0000

    add likes and flags column

--- db/populatedb.js
+++ db/populatedb.js
@@ -2,33 +2,38 @@ require("dotenv").config();
 const { Client } = require("pg");
 
 const SQL = `
-CREATE TABLE IF NOT EXISTS messages (
+DROP TABLE IF EXISTS messages;
+
+CREATE TABLE messages (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   text TEXT NOT NULL,
   "user" VARCHAR(255) NOT NULL,
+  likes INTEGER DEFAULT 0,
+  flags INTEGER DEFAULT 0,
   added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO messages (text, "user") 
+INSERT INTO messages (text, "user", likes, flags) 
 VALUES
-  ('Hi there!', 'Amando'),
-  ('Hello World!', 'Charles'),
-  ('PostgreSQL makes this so much better.', 'Ash');
+  ('Hi there!', 'Amando', 5, 0),
+  ('Hello World!', 'Charles', 2, 1),
+  ('PostgreSQL makes this so much better.', 'Ash', 10, 0),
+  ('Spam message that people are reporting.', 'Spammer99', 0, 4);
 `;
 
 async function main() {
-  console.log("Seeding db...");
+  console.log("Seeding database...");
 
   const client = new Client({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DB_URL,
   });
 
   try {
     await client.connect();
     await client.query(SQL);
-    console.log("Db seeded successfully!");
+    console.log("Database seeded successfully!");
   } catch (error) {
-    console.error("Error seeding db:", error);
+    console.error("Error seeding database:", error);
   } finally {
     await client.end();
   }
